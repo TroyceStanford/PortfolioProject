6eabd0c885b0268cb5d3a42fa76be0adc41575cf
TroyceStanford <troyce.stanford@gmail.com>
2025-07-22 17:32:28 +0100

    Ore Inventory

--- PortfolioProject/Assets/Scripts/Inventory.cs
+++ PortfolioProject/Assets/Scripts/Inventory.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public List<GameObject> _Items = new List<GameObject>();
+    public List<GameObject> _CopperOres = new List<GameObject>();
+    public GameObject _CopperBox;
 
     
     void Start()
@@ -13,6 +14,15 @@ public class Inventory : MonoBehaviour
 
     void Update()
     {
-        
+        if (_CopperOres.Count >= 1 && _CopperOres[0]==null)
+        {
+            _CopperOres.RemoveAt(0);
+            if(_CopperOres.Count > 0)
+            {
+                Vector3 pos = new Vector3(_CopperBox.transform.position.x, _CopperBox.transform.position.y + 0.5f, _CopperBox.transform.position.z);
+                _CopperOres[0].transform.position = pos;
+                _CopperOres[0].SetActive(true);
+            }
+        }
     }
 }
--- PortfolioProject/Assets/Scripts/Storage.cs
+++ PortfolioProject/Assets/Scripts/Storage.cs
@@ -8,6 +8,7 @@ public class Storage : MonoBehaviour
     public GameObject _ShopPos;
     public GameObject _StoragePos;
     public GameObject _DropPos;
+    public Inventory _Inventory;
 
     void Start()
     {
@@ -38,8 +39,23 @@ public class Storage : MonoBehaviour
     {
         foreach(var item in _StoredItems)
         {
-            item.transform.position = _DropPos.transform.position;
-            item.SetActive(true);
+            DragAndDrop dragAndDrop = item.GetComponent<DragAndDrop>();
+            if(dragAndDrop._Item._Name == "Copper Ore")
+            {
+                if(_Inventory._CopperOres.Count == 0)
+                {
+                    _Inventory._CopperOres.Add(item);
+                    Vector3 pos = new Vector3(_Inventory._CopperBox.transform.position.x, _Inventory._CopperBox.transform.position.y + 0.5f, _Inventory._CopperBox.transform.position.z);
+                    _Inventory._CopperOres[0].transform.position = pos;
+                    _Inventory._CopperOres[0].SetActive(true);
+                }
+                else
+                {
+                    _Inventory._CopperOres.Add(item);
+                }
+            }
+            //item.transform.position = _DropPos.transform.position;
+            //item.SetActive(true);
         }
         _StoredItems.Clear();
     }
